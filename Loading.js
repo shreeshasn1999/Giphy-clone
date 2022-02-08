d999119946841ae274ee4402be0fe1b474d2e4f1
shreeshasn1999 <48211496+shreeshasn1999@users.noreply.github.com>
2022-02-08 23:50:18 +0530

    Delete Loading.js

--- Loading.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-function Loading() {
-  return (
-    <div className="loading">
-      <span className="loading-span">Loading...</span>
-    </div>
-  );
-}
-export default Loading;
